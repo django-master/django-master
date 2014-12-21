a34b33c7e1e63b6b696e297c270f2b5ce74aefdb
Andre Farzat <andrefarzat@gmail.com>
2014-12-21 16:38:59 -0500

    removing references of django-master-linter

--- djangomaster/conf.py
+++ djangomaster/conf.py
@@ -1,7 +1,5 @@
 import os
-import re
 from md5 import md5
-from subprocess import Popen, PIPE
 
 from django.conf import settings as djangosettings
 from django.core.exceptions import ImproperlyConfigured
@@ -12,14 +10,6 @@ TEST_STATIC_FOLDER = os.path.join(djangosettings.STATIC_ROOT, 'djangomaster',
 
 TEST_STATIC_FOLDER = getattr(djangosettings, 'TEST_STATIC_FOLDER',
                              TEST_STATIC_FOLDER)
-
-
-def _is_installed(cmd):
-    try:
-        resp = Popen(['which', cmd], stdout=PIPE)
-    except OSError:
-        return False
-    return resp.stdout.read().strip().endswith(cmd)
 
 
 def _create_whole_path_to_folder(path, start_from=''):
@@ -40,16 +30,16 @@ def _create_whole_path_to_folder(path, start_from=''):
             os.mkdir(path)
 
 
-class Settings():
+class Settings(object):
     conf = djangosettings
 
-    def _get(self, prop_name, default_value):
+    def get(self, prop_name, default_value):
         prop_name = 'DJANGOMASTER_%s' % prop_name
         return getattr(self.conf, prop_name, default_value)
 
     @property
     def BASE_DIR_NAME(self):
-        return self._get('BASE_DIR', 'BASE_DIR')
+        return self.get('BASE_DIR', 'BASE_DIR')
 
     @property
     def BASE_DIR(self):
@@ -57,37 +47,7 @@ class Settings():
 
     @property
     def REQUIREMENTS_TXT(self):
-        return self._get('REQUIREMENTS_NAME', 'requirements.txt')
-
-    @property
-    def PYLINT_CMD(self):
-        return self._get('PYLINT_CMD', 'pep8')
-
-    @property
-    def PYLINT_IS_INSTALLED(self):
-        return _is_installed(self.PYLINT_CMD)
-
-    @property
-    def JSLINT_CMD(self):
-        return self._get('JSLINT_CMD', 'jshint')
-
-    @property
-    def JSLINT_IS_INSTALLED(self):
-        return _is_installed(self.JSLINT_CMD)
-
-    @property
-    def PYLINT_IGNORED_PATTERNS(self):
-        ret = []
-        for value in self._get('PYLINT_INGORED_PATTERNS', (r'/migrations/', )):
-            ret.append(re.compile(value))
-        return ret
-
-    @property
-    def JSLINT_IGNORED_PATTERNS(self):
-        ret = []
-        for value in self._get('JSLINT_INGORED_PATTERNS', ()):
-            ret.append(re.compile(value))
-        return ret
+        return self.get('REQUIREMENTS_NAME', 'requirements.txt')
 
     @property
     def SOUTH_IS_INSTALLED(self):
@@ -95,7 +55,7 @@ class Settings():
 
     @property
     def SIGNAL_MODULES(self):
-        modules = self._get('SIGNAL_MODULES', ())
+        modules = self.get('SIGNAL_MODULES', ())
         if type(modules) not in (tuple, list):
             raise ImproperlyConfigured('DJANGOMASTER_SIGNAL_MODULES must be '
                                        'a tuple')
